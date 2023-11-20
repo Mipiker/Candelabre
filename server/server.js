a8d72a5e70d103dd765876e7d2412c39cce9f1e6
abdchn <chaneabde@icloud.com>
2023-11-20 09:53:55 +0100

    add some swaggg

---
+++ server/server.js
@@ -0,0 +1,70 @@
+var button = document.querySelector('#frqmax');
+
+        // Ajouter un écouteur d'événement pour détecter le survol
+        button.addEventListener('mouseover', function() {
+            button.style.backgroundColor = "skyblue";
+            button.style.color = "orange";
+        });
+
+        // Ajouter un écouteur d'événement pour détecter la sortie du survol
+        button.addEventListener('mouseout', function() {
+            button.style.backgroundColor = "";
+            button.style.color = "";
+        });
+
+var button1 = document.querySelector('#pui');
+
+        // Ajouter un écouteur d'événement pour détecter le survol
+        button1.addEventListener('mouseover', function() {
+            button1.style.backgroundColor = "skyblue";
+            button1.style.color = "orange";
+        });
+
+        // Ajouter un écouteur d'événement pour détecter la sortie du survol
+        button1.addEventListener('mouseout', function() {
+            button1.style.backgroundColor = "";
+            button1.style.color = "";
+        });
+        
+    var button2 = document.querySelector('#amp');
+
+        // Ajouter un écouteur d'événement pour détecter le survol
+        button2.addEventListener('mouseover', function() {
+            button2.style.backgroundColor = "skyblue";
+            button2.style.color = "orange";
+        });
+
+        // Ajouter un écouteur d'événement pour détecter la sortie du survol
+        button2.addEventListener('mouseout', function() {
+            button2.style.backgroundColor = "";
+            button2.style.color = "";
+        });
+        
+var button3 = document.querySelector('#anomalies');
+
+        // Ajouter un écouteur d'événement pour détecter le survol
+        button3.addEventListener('mouseover', function() {
+            button3.style.backgroundColor = "skyblue";
+            button3.style.color = "orange";
+        });
+
+        // Ajouter un écouteur d'événement pour détecter la sortie du survol
+        button3.addEventListener('mouseout', function() {
+            button3.style.backgroundColor = "";
+            button3.style.color = "";
+        });
+
+var button4 = document.querySelector('#devices');
+
+        // Ajouter un écouteur d'événement pour détecter le survol
+        button4.addEventListener('mouseover', function() {
+            button4.style.backgroundColor = "skyblue";
+            button4.style.color = "orange";
+        });
+
+        // Ajouter un écouteur d'événement pour détecter la sortie du survol
+        button4.addEventListener('mouseout', function() {
+            button4.style.backgroundColor = "";
+            button4.style.color = "";
+        });
+        
