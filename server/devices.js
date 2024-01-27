809cc8c61efe11a86de4aa32e90253ee302fc387
Mipiker <antoine.b44@outlook.fr>
2024-01-28 00:11:43 +0100

    add device preselection on page loading with URL

--- server/devices.js
+++ server/devices.js
@@ -3,36 +3,59 @@ import {csvJSON} from './readLogManager.js'
 var selectedDevice = document.getElementById('device-selection');
 var lastConnection = document.createElement("p");
 
-document.addEventListener("DOMContentLoaded", function () {
-    generateEUIList("dynamic-device-list");
+// When the page is loaded
+document.addEventListener("DOMContentLoaded", async function () {
+    // Generate the device list
+    await generateEUIList();
+    // Preselect a device is needed
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedDeviceEUI = urlParams.get('deviceEUI');
+    if (selectedDeviceEUI) {
+        preselectDevice(selectedDeviceEUI);
+    }
 });
 
-// Generate the device list dynamically from the file devices.csv
-async function generateEUIList(containerId) {
-
+// Generate the device list dynamically from the file devices.csv when the page is loaded
+async function generateEUIList() {
     var devices = await csvJSON('../log/devices.csv');
-
-    var container = document.getElementById(containerId);
-
+    var container = document.getElementById("dynamic-device-list");
     var ul = document.createElement("ul");
-
     for(const device of devices) {
         var li = document.createElement("li");
         li.textContent = device.EUI;
         li.addEventListener("click", function() {
-            deviceClicked(device)
+            deviceSelected(device)
         });
         ul.appendChild(li);
     }
-
     container.appendChild(ul);
 }
 
-async function deviceClicked(device) {
-    console.log(device);
+// Display the device informations when selected
+async function deviceSelected(device) {
     var deviceInfo = document.getElementById('device-info');
     var deviceLog = await csvJSON(`../log/${device.EUI}.csv`)
     selectedDevice.textContent = `Informations de l'appareil ${device.EUI}`;
     lastConnection.textContent = `Dernière connection le ${deviceLog.slice(-1)[0].date}`;
     deviceInfo.appendChild(lastConnection);
+    updateURLParameter('deviceEUI', device.EUI);
+}
+
+// Preselect the device based on the URL parameter
+function preselectDevice(deviceEUI) {
+    const devicesList = document.getElementById("dynamic-device-list").getElementsByTagName("li");
+    for (const deviceElement of devicesList) {
+        if (deviceElement.textContent === deviceEUI) {
+            deviceElement.click();
+            break;
+        }
+    }
+}
+
+// Update a URL parameter
+function updateURLParameter(key, value) {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(key, value);
+    const newURL = window.location.pathname + '?' + urlParams.toString();
+    window.history.replaceState({}, '', newURL);
 }
