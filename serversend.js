ab3bfbeb3dabadd77cb70c129503a2877f0b95d9
abdchn <chaneabde@icloud.com>
2024-01-27 14:55:24 +0100

    attempt update sreversend

--- serversend.js
+++ serversend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000; // defini le port à utiliser
 
-app.listen(port, () => {
+app.listen(port, () => {  
   console.log(`Server is running on port ${port}`);
 });
 
